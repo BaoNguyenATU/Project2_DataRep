e4ce77c6506ed8fe4aaeb652ec39eb8ee2561ace
Bao Nguyen <g00420095@atu.ie>
2024-12-10 09:32:48 +0000

    testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-    return <h3>Welcome to the healthyiest lifestyle on the internet</h3>;
+    return <h3>Welcome to the healthiest lifestyle on the internet</h3>;
   };
   
   export default Footer;
