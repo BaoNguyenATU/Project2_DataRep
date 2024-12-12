753c88e8f0f101670b81c11b96a59f863dc196ce
Bao Nguyen <g00420095@atu.ie>
2024-12-12 14:29:14 +0000

    testing

--- src/components/EditMeals.js
+++ src/components/EditMeals.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const EditMeals = () => {
     const { id } = useParams();
